cb0dc8889dcd2db6719f0f063eadae48047e5d28
Pavel Rosický <pavel.rosicky@easy.cz>
2021-11-02 17:41:31 +0100

    routes with dots

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'rys_management/:rys_id/edit', to: 'rys_management#edit', as: 'rys_management_edit'
-  put 'rys_management/:rys_id/update', to: 'rys_management#update', as: 'rys_management_update'
+  get 'rys_management/:rys_id/edit', to: 'rys_management#edit', as: 'rys_management_edit', rys_id: /[^\/]+/
+  put 'rys_management/:rys_id/update', to: 'rys_management#update', as: 'rys_management_update', rys_id: /[^\/]+/
   post 'rys_management/toggle_feature(/:id)', to: 'rys_management#toggle_feature', as: 'rys_management_toggle_feature'
 
 end
